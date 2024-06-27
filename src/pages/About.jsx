2af3613b2ea4901972701c9cc72dd3653acf2aa7
donabinu921 <elizabethdona921@gmail.com>
2024-06-28 00:50:22 +0530

    About,ContactMe set up initial

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import "../styles/About.css"
+import React from 'react';
+import '../styles/About.css';
+import "../styles/BouncingBall.css";
 
 const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about" className="about-section">
+      <h2>Get to know me!</h2>
+      <p className='about-p'>Hey there, I'm a passionate Front-End Developer and Designer based in Kerala, India. With a
+keen focus on creating captivating user experiences, I'm 
+experienced in developing simple but efficient
+solutions using technologies including HTML, CSS,
+JavaScript, React.js and Figma. I'm eagerly seeking opportunities to collaborate
+with like-minded professionals and contribute to
+projects prioritizing innovation and personal and
+professional development.</p>
+      <div className="bouncing-ball"></div>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "../styles/ContactMe.css"
+import { AiFillLinkedin, AiFillMail, AiFillGithub, AiFillFile } from "react-icons/ai";
 
 const ContactMe = () => {
   return (
-    <section id='contactme'>ContactMe</section>
+    <section id='contactme'>
+      <div className='socials-container'>
+      <a href="https://www.linkedin.com/in/donabinu" target="_blank" rel="noopener noreferrer">
+          <AiFillLinkedin className='socials-icon' />
+        </a>
+        <a href="mailto:donabinu06@gmail.com" target="_blank" rel="noopener noreferrer">
+          <AiFillMail className='socials-icon' />
+        </a>
+        <a href="https://github.com/donabinu921" target="_blank" rel="noopener noreferrer">
+          <AiFillGithub className='socials-icon' />
+        </a>
+        <a href="https://flowcv.com/resume/suv5ffmgcd" target="_blank" rel="noopener noreferrer">
+          <AiFillFile className='socials-icon' />
+        </a>
+      </div>
+      <p className='copyright'>© {new Date().getFullYear()} Dona Binu. All rights reserved.</p>
+    </section>
   )
 }
 
