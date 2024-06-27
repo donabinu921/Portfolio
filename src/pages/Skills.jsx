301101b1543aae721466c36bd36194e8f7d80773
donabinu921 <elizabethdona921@gmail.com>
2024-06-27 22:54:31 +0530

    Skills Page set up initial

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import "../styles/Skills.css"
+import { FaDatabase } from "react-icons/fa6";
+import { IoLogoFigma, IoLogoHtml5, IoLogoCss3, IoLogoJavascript, IoLogoReact, IoLogoPython } from "react-icons/io5";
 
 const Skills = () => {
   return (
-    <section id='skills'>Skills</section>
+    <section id='skills' className='skills-page'>
+      <div className='skills-container'>
+        <div className='skill'>
+          <IoLogoHtml5 className='skills-icon'/>
+          <h3>HTML5</h3>
+        </div>
+        <div className='skill'>
+          <IoLogoCss3 className='skills-icon'/>
+          <h3>CSS3</h3>
+        </div>
+        <div className='skill'>
+          <IoLogoJavascript className='skills-icon'/>
+          <h3>JS</h3>
+        </div>
+        <div className='skill'>
+          <IoLogoReact className='skills-icon'/>
+          <h3>REACT</h3>
+        </div>
+        <div className='skill'>
+          <IoLogoPython className='skills-icon'/>
+          <h3>PYTHON</h3>
+        </div>
+        <div className='skill'>
+          <IoLogoFigma className='skills-icon'/>
+          <h3>FIGMA</h3>
+        </div> 
+        <div className='skill'>
+          <FaDatabase className='skills-icon'/>
+          <h3>DBMS</h3>
+        </div> 
+      </div>
+    </section>
   )
 }
 
